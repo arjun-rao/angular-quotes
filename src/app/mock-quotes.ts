96384bce18e21c2d29046724dff247797e1a50ee
arjun-rao <mailarjunrao@gmail.com>
2016-10-01 00:29:20 +0530

    added 404

--- src/app/mock-quotes.ts
+++ src/app/mock-quotes.ts
@@ -3,13 +3,13 @@ import { Quote } from './quote';
 
 export const QUOTES: Quote[] = [  
   {id:1,text: 'Innovation distinguishes between a leader and a follower', byline: 'Steve Jobs'},
-  { id: 2,text:'Foo', byline: 'Narco' },
-  { id: 3,text:'Bar', byline: 'Bombasto' },
-  { id: 4,text:'Baz', byline: 'Celeritas' },
-  { id: 5,text:'Cat', byline: 'Magneta' },
-  { id: 6,text:'Dog', byline: 'RubberMan' },
-  { id: 7,text:'Mouse', byline: 'Dynama' },
-  { id: 8,text:'Rabbit', byline: 'Dr IQ' },
-  { id: 9,text:'Fox', byline: 'Magma' },
-  { id: 10,text:'Owl', byline: 'Tornado' }
+  { id: 2,text:'Moral indignation is jealousy with a halo', byline: 'H. G. Wells' },
+  { id: 3,text:'Political correctness is tyranny with manners', byline: 'Charlton Heston ' },
+  { id: 4,text:'You can avoid reality, but you cannot avoid the consequences of avoiding reality', byline: 'Ayn Rand' },
+  { id: 5,text:'The only way to get rid of a temptation is to yield to it', byline: 'Oscar Wilde' },
+  { id: 6,text:'There are no facts, only interpretations', byline: 'Friedrich Nietzsche' },
+  { id: 7,text:'Dancing is silent poetry', byline: 'Simonides' },
+  { id: 8,text:'He who has a "why" to live, can bear with almost any "how"', byline: 'Friedrich Nietzsche' },
+  { id: 9,text:'Be nice to people on your way up because you meet them on your way down', byline: 'Jimmy Durante' },
+  { id: 10,text:'Life is pleasant. Death is peaceful. It\'s the transition that\'s troublesome', byline: 'Isaac Asimov' }
 ];
